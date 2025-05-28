11b783fd821a9c3a30ae9c754f10c1fb0efe6629
martin-gondek <martingondekjuniorik@gmail.com>
2025-05-28 11:50:06 +0200

    .

--- src/main/java/com/example/orchesterfx/HelloApplication.java
+++ src/main/java/com/example/orchesterfx/HelloApplication.java
@@ -16,7 +16,7 @@ public class HelloApplication extends Application {
         stage.setScene(scene);
         stage.show();
     }
-    //tu daco je
+    //opa
 
     public static void main(String[] args) {
         launch();
